d16c1f89fbdedd516306fad0e94cc5b64fc74ec4
Carl Scotney <carl.scotney@gmail.com>
2020-06-01 10:19:48 +0100

    Test shell files added

--- test/Chartist.Blazor.UnitTests/Core/Core/Data/SeriesPointShould.cs
+++ test/Chartist.Blazor.UnitTests/Core/Core/Data/SeriesPointShould.cs
@@ -7,18 +7,6 @@ namespace Chartist.Blazor.UnitTests.Core.Core.Data
 {
     public class SeriesPointShould
     {
-        [Fact]
-        public void ToStringForJsonCorrectly()
-        {
-        //Given
-        var sut = new SeriesPoint(){
-            Y=1.00,
-            Meta= new PointMetaInfo{Label="label"}
-        };
-            //When
-            var compare = JsonSerializer.Serialize(sut);
-        //Then
-
-        }
+        
     }
 }
---
+++ test/Chartist.Blazor.UnitTests/Core/Core/Data/TestDataFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Chartist.Blazor.Core.Data;
+
+namespace Chartist.Blazor.UnitTests.Core.Core.Data
+{
+    public static class TestDataFactory
+    {
+        
+        public static ExtendedChartData UnlabelledData()
+        {
+            var result = new ExtendedChartData()
+            {
+
+                Labels = new List<string> { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" }
+
+            };
+
+
+            var series = new Series();
+            series.Data.Add(new SeriesPoint { Y = 12 });
+            series.Data.Add(new SeriesPoint { Y = 9 });
+            series.Data.Add(new SeriesPoint { Y = 7 });
+            series.Data.Add(new SeriesPoint { Y = 8 });
+            series.Data.Add(new SeriesPoint { Y = 5 });
+            result.Series.Add(series);
+
+            return result;
+        }
+    }
+}
